004629f7c22cdfb8cc21e1bab06d7305f3bb6343
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-11-14 22:20:10 +0100

    Update Hell_Week_01.py

--- Hell_Week_01.py
+++ Hell_Week_01.py
@@ -9,6 +9,62 @@
 
 import random 
 
-x = random.randrange(1, 256)
+def Home():
+    print("I have a number from 1 to 256. Do you want to take a guess?! (Yes/No)")
+    Answer(input())
 
-print(x)
+def Answer(Yes_Or_No):
+    
+    Answer = Yes_Or_No.capitalize()
+
+    try:
+        if(Answer == "Yes"):
+            Count = 0
+            Random_Number = random.randrange(1, 257)
+            print("Now take your guess:")
+            Counting = Count + 1
+            try:
+                Guessed_Number = int(input())
+                Game(Random_Number, Guessed_Number, Counting)
+            except:
+                SmartAss()
+
+        elif(Answer == "No"):
+            print("OK, goodbye!")
+        else:
+            SmartAss()
+    except:
+        SmartAss()
+    
+def SmartAss():
+    print("That's not what I've asked for!!! Do you really want to play? (Yes/No)")
+    Answer(input())
+
+def Game(Random_Number, Guessed_Number, Counting):
+
+    try:
+
+        if(Guessed_Number < Random_Number):
+            print("It's to Small, sorry... Try again!!!")
+            New_Guessed_Number = int(input())
+            New_Counting = Counting + 1
+            Game(Random_Number, New_Guessed_Number, New_Counting)
+
+        elif(Guessed_Number > Random_Number):
+            print("It's to Big, sorry... Try again!!!")
+            New_Guessed_Number = int(input())
+            New_Counting = Counting + 1
+            Game(Random_Number, New_Guessed_Number, New_Counting)
+
+        elif(Guessed_Number == Random_Number):
+            print("Wow, you guessed it!!! And it took you only:", Counting, "times !!!")
+            print("Do you want to play again? (Yes/No)")
+            Answer(input())
+
+    except:
+        print("That's a wrong answer. Try again!")
+        New_Guessed_Number = int(input())
+        New_Counting = Counting + 1
+        Game(Random_Number, New_Guessed_Number, New_Counting)
+
+Home()
