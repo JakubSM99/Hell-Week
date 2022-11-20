501904e4b634c5fd2011366be334cc059cad05f3
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-11-20 18:36:49 +0100

    Zadanie 06

--- Hell_Week_2022/Day_06/hell_week_06.py
+++ Hell_Week_2022/Day_06/hell_week_06.py
@@ -9,27 +9,6 @@
 
 # Dodatkowo niech program sprawdzi czy wszystkie wartości w kwadracie są unikalne.
 
-# def suma_w_lini(lista):
-#     suma = 0
-#     for var in lista:
-#         suma += int(var)
-#     return suma
-
-
-# def suma_w_kolumnie(lista1, lista2, lista3, kolumna:int):
-#     a = kolumna-1
-#     suma = int(lista1[a]) + int(lista2[a]) + int(lista3[a])
-#     return suma 
-
-# def suma_przekatnej00x22(lista1, lista2, lista3):
-#     suma = int(lista1[0]) + int(lista2[1]) + int(lista3[2])
-#     return suma
-
-
-# def suma_przekatnej20x02(lista1, lista2, lista3):
-#     suma = int(lista3[0]) + int(lista2[1]) + int(lista1[2])
-#     return suma
-
 def spr_kolumn(lista1, lista2, lista3):
     kolumna_1 = int(lista1[0]) + int(lista2[0]) + int(lista3[0])
     kolumna_2 = int(lista1[1]) + int(lista2[1]) + int(lista3[1])
@@ -37,7 +16,7 @@ def spr_kolumn(lista1, lista2, lista3):
 
     if kolumna_1 == kolumna_2 and kolumna_1 == kolumna_3:
         suma_kolumn = kolumna_1 + kolumna_2 +kolumna_3
-        print(suma_kolumn)
+        # print(suma_kolumn)
         return suma_kolumn
     else:
         a = "Kwadrat nie jest magiczny"
@@ -57,25 +36,44 @@ def spr_lini(lista1, lista2, lista3):
     
     if suma1 == suma2 and suma1 == suma3:
         suma_wierszy = suma1 + suma2 + suma3
-        print(suma_wierszy)
+        # print(suma_wierszy)
         return suma_wierszy
     else:
         a = "Kwadrat nie jest magiczny"
         return a
     
 def spr_przekatnych(lista1, lista2, lista3):
+    
     suma1 = int(lista1[0]) + int(lista2[1]) + int(lista3[2])
     suma2 = int(lista3[0]) + int(lista2[1]) + int(lista1[2])
 
     if suma1 == suma2:
         przekatne = suma1*3
-        print(przekatne)
+        # print(przekatne)
         return przekatne
     else:
         a = "Kwadrat nie jest magiczny"
         return a
 
-with open("C:/Users/Jakub/Documents/GitHub/Hell-Week/Hell_Week_2022/Day_06/magic_square_1.txt") as f:
+def unikalne(lista):
+    if len(set(lista)) == len(lista):
+        print("Wartosci kwadratu sa unikalne")
+    else:
+        print("Wartosci kwadratu nie sa unikalne")
+
+def kwadrat_magiczny(lista1, lista2, lista3):
+    if spr_kolumn(lista1, lista2, lista3) == spr_lini(lista1, lista2, lista3):
+        if spr_kolumn(lista1, lista2, lista3) == spr_przekatnych(lista1, lista2, lista3):
+            if spr_kolumn(lista1, lista2, lista3) != "Kwadrat nie jest magiczny":
+                print("kwadrat jest Magiczny")
+            else:
+                print("Kwadrat nie jest magiczny")
+        else:
+            print("Kwadrat nie jest magiczny")
+    else:
+        print("Kwadrat nie jest magiczny")
+
+def dzialania_na_kwadracie(f):
     
     list_a = []
     lista1 = []
@@ -98,33 +96,15 @@ with open("C:/Users/Jakub/Documents/GitHub/Hell-Week/Hell_Week_2022/Day_06/magic
     for a in list_a[6:]:
         lista3.append(a)
 
-    if spr_kolumn(lista1, lista2, lista3) == spr_lini(lista1, lista2, lista3):
-        if spr_kolumn(lista1, lista2, lista3) == spr_przekatnych(lista1, lista2, lista3):
-            if spr_kolumn != "Kwadrat nie jest magiczny":
-                print("kwadrat jest Magiczny")
-            else:
-                print("Kwadrat nie jest magiczny")
-        else:
-            print("Kwadrat nie jest magiczny")
-    else:
-        print("Kwadrat nie jest magiczny")
+    kwadrat_magiczny(lista1, lista2, lista3)
 
-# kolumna_1 = suma_w_kolumnie(lista1, lista2, lista3, 1)
-# kolumna_2 = suma_w_kolumnie(lista1, lista2, lista3, 2)
-# kolumna_3 = suma_w_kolumnie(lista1, lista2, lista3, 3)
+    unikalne(list_a)
 
-# linia_1 = suma_w_lini(lista1)
-# linia_2 = suma_w_lini(lista2)
-# linia_3 = suma_w_lini(lista3)
 
-# przekatna_1 = suma_przekatnej00x22(lista1, lista2, lista3)
-# przekatna_2 = suma_przekatnej20x02(lista1, lista2, lista3)
+with open("C:/Users/Jakub/Documents/GitHub/Hell-Week/Hell_Week_2022/Day_06/magic_square_1.txt") as f:
+    print("Odp. Dla 'Magic_square_1.txt' : ")
+    dzialania_na_kwadracie(f)
 
-# print(kolumna_1)
-# print(kolumna_2)
-# print(kolumna_3)
-# print(linia_1)
-# print(linia_2)
-# print(linia_3)
-# print(przekatna_1)
-# print(przekatna_2)
+with open("C:/Users/Jakub/Documents/GitHub/Hell-Week/Hell_Week_2022/Day_06/magic_square_2.txt") as f:
+    print("Odp. Dla 'Magic_square_2.txt' : ")
+    dzialania_na_kwadracie(f)
